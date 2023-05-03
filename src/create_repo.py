8490458faa86fd00420e7e2ee38ffeae8de97fa2
Mira Lee <leemira1994@gmail.com>
2023-05-03 22:41:21 +0100

    changed default color

--- src/create_repo.py
+++ src/create_repo.py
@@ -14,6 +14,7 @@ def create_github_repository(g, repo_name):
         return repo
     except GithubException as e:
         print(f'Error creating repository: {e}')
+        exit()
 
 # Creates a label
 def create_issue_label(g, repo, label_name, color):
@@ -21,10 +22,11 @@ def create_issue_label(g, repo, label_name, color):
         label = g.get_user().get_repo(repo).create_label(label_name, color)
         return label
     except GithubException as e:
-        print(f'Error color: {e}')
+        print(f'Error creating label: {e}')
+        exit()
 
-
-def validate_color(color="pink"):
+# Validare color input
+def validate_color(color):
     colors = {
     "red": "FF0000",
     "green": "00FF00",
@@ -33,7 +35,6 @@ def validate_color(color="pink"):
     "black": "000000",
     "pink": "FFC0CB"
     }
-
     if color.lower() in colors:
         color = colors[color].lower()
         return color
@@ -45,24 +46,28 @@ def validate_color(color="pink"):
         exit()
 
 def main():
+    # Assign module utils to a variable
     u = utils
+
     # Parse command-line arguments
     args = u.parse_args()
+
+    # Validate color input
     color = validate_color(args.color)
+
     # Get GitHub access token from a config file
     token = u.get_github_token()
+
+    # Create Github object with token as a parameter
     g = Github(token)
 
     # Create the new repository
     create_github_repository(g, args.repo)
+
     # Add the specified team as a label for the repository
     if args.label:
-        if args.color:
-            create_issue_label(g, args.repo, args.label, color)
-            print(f"Label created with team name {args.label} and color {args.color}")
-        if args.color is None:
-            create_issue_label(g, args.repo, args.label)
-            print(f"Label created with team name {args.label}")
+        create_issue_label(g, args.repo, args.label, color)
+        print(f"Label created with team name {args.label} and color {args.color}")
 
 if __name__ == '__main__':
     main()
--- src/delete_repo.py
+++ src/delete_repo.py
@@ -1,15 +1,20 @@
 from github import Github
 import utils
 
-
+# Delete created repository
 def delete_repo(repo, token):
     g = Github(token)
     g.get_user().get_repo(repo).delete()
 
 
 def main():
+    # Save module utils to a variable
     u = utils
+
+    # Parse command-line arguments
     args = u.parse_args()
+
+    # Get GitHub access token from a config file
     token = u.get_github_token()
     delete_repo(args.repo, token)
 
